3f52e91ce5424a48397ffd440b4c9199fc825402
Mike Plant <26470248+mikeplant@users.noreply.github.com>
2023-05-23 14:35:26 +0100

    Add functions to allow resizing of grid via slider

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 
-let gridSquare;
+
+const sizeSlider = document.querySelector('#board-size');
+let gridSize = 12;
 
 function createGrid() {
   const gridContainer = document.querySelector('.grid-container');
-  for (let i = 0; i < 256; i++) {
-    gridSquare = document.createElement('div');
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+  for (let i = 0; i < (gridSize * gridSize); i++) {
+    let gridSquare = document.createElement('div');
     gridSquare.classList.add('grid-square');
     gridContainer.appendChild(gridSquare);
 
@@ -17,6 +22,22 @@ function createGrid() {
 function fillColour(selectedSquare) {
   selectedSquare.classList.add('filled-square');
 }
+
+function resizeGrid() {
+  let gridSquares = document.querySelectorAll('.grid-square');
+  gridSquares.forEach(square => {
+    square.remove();
+  });
+  createGrid();
+}
+
+sizeSlider.addEventListener('input', (e) => {
+  const sizeDisplay = document.querySelector('#size-display');
+  const value = e.target.value;
+  sizeDisplay.textContent = `${value} x ${value}`;
+  gridSize = value;
+  resizeGrid();
+});
 
 createGrid();
 

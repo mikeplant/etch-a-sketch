da3e8dce209d691f8b1da33b2873a0762a656a2f
Mike Plant <26470248+mikeplant@users.noreply.github.com>
2023-05-23 11:58:40 +0100

    Add eventListener and fillColour function to fill squares on mouseover event

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 
+let gridSquare;
+
 function createGrid() {
   const gridContainer = document.querySelector('.grid-container');
-  console.log(gridContainer)
   for (let i = 0; i < 256; i++) {
-    let gridSquare = document.createElement('div');
+    gridSquare = document.createElement('div');
     gridSquare.classList.add('grid-square');
-    gridSquare.classList.add(`grid-ref-${i+1}`);
     gridContainer.appendChild(gridSquare);
+
+    gridSquare.addEventListener('mouseover', (e)=> {
+      fillColour(e.target);
+    });
   }
 }
 
+function fillColour(selectedSquare) {
+  selectedSquare.classList.add('filled-square');
+}
+
 createGrid();
+
